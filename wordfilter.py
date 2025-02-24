7af44938fe4a3e0cfd51aa8c0926daa965089771
msanch023 <mas023@ucsd.edu>
2025-02-24 12:53:40 -0800

    created word filtering script and filtered incorrect and incorrect_advance

---
+++ wordfilter.py
@@ -0,0 +1,15 @@
+# %%
+import nltk
+from nltk.corpus import words
+
+nltk.download('words')
+
+eng = set(words.words())
+
+with open("incorrect_advance.txt", "r+")as file:
+    wrd = [line.strip() for line in file]
+    wrd = [seq for seq in wrd if seq.lower() not in eng]
+
+    file.seek(0)
+    file.truncate()
+    file.write("\n".join(wrd))
